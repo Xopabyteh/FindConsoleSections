f491c4d4c2fddfa9ff28a60ed0b8f3ba5c2b25e9
Xopabyteh <70832276+Xopabyteh@users.noreply.github.com>
2021-12-19 20:01:32 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿#nullable enable
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        private static void Main()
+        {
+            
+            string? projectsPath = string.Empty; //@"D:\!VisualStudioProjects\source"
+            while (string.IsNullOrEmpty(projectsPath))
+            {
+                Console.Write("Your projects path: ");
+                projectsPath = Console.ReadLine();
+            }
+
+            IEnumerable<string> csFiles = FindCsFiles(projectsPath);
+            StringBuilder builder = new StringBuilder();
+            foreach (var csFile in csFiles)
+            {
+                string? consoleSections = FindConsoleSections(csFile);
+                if (!string.IsNullOrWhiteSpace(consoleSections))
+                {
+                    builder.AppendLine(csFile).AppendLine(consoleSections);
+                }
+                
+            }
+
+            Console.WriteLine(builder.ToString());
+        }
+
+        private static string? FindConsoleSections(string csFilePath)
+        {
+            string data = string.Empty;
+            try
+            {
+                data = File.ReadAllText(csFilePath);
+            }
+            catch
+            {
+                return null;
+            }
+            string[] lines = data.Split(Environment.NewLine);
+            StringBuilder builder = new StringBuilder();
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                line = line.TrimStart();
+                if (line.Contains("Console"))
+                {
+                    builder.Append("\tLine ").Append(i + 1).Append(" - ").AppendLine(line);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        private static IEnumerable<string> FindCsFiles(string path)
+        { 
+            IEnumerable<string> csFiles = Directory.EnumerateFileSystemEntries(path, "*.cs", SearchOption.AllDirectories);
+            return csFiles;
+        }
+            
+    }
+}
